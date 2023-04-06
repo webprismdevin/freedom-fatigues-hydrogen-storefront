3ee63a62c0974ac27096bce05a2c4b18a5f82f04
Devin Santamaria <devin@webprism.co>
2023-04-06 08:37:50 -0700

    marquee ready to integrate Sanity

--- app/components/Marquee.tsx
+++ app/components/Marquee.tsx
@@ -1,48 +1,59 @@
 export default function Marquee() {
   return (
     <div>
-      <div className="overflow-hidden py-3">
-        <div>
-          <div className="flex h-12 w-full items-center justify-center gap-3 text-white">
-            <span className="font-heading text-4xl">American Made</span>
-            <svg
-              width="36px"
-              height="36px"
-              strokeWidth="1.5"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              color="white"
-            >
-              <path
-                d="M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243"
-                stroke="white"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>
-            </svg>
-            <span className="font-heading text-4xl">Veteran Owned</span>
-            <svg
-              width="36px"
-              height="36px"
-              strokeWidth="1.5"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              color="white"
-            >
-              <path
-                d="M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243"
-                stroke="white"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>
-            </svg>
-          </div>
-        </div>
+      <div className="align-center flex justify-start gap-x-4 overflow-hidden py-3">
+        <MarqueeContent />
+        <MarqueeContent />
+        <MarqueeContent />
+        <MarqueeContent />
       </div>
+    </div>
+  );
+}
+
+function MarqueeContent() {
+  return (
+    <div className="justify-space-between scroll flex h-12 w-full items-stretch gap-x-4 uppercase text-white">
+      <span className="whitespace-nowrap font-heading text-4xl">
+        American Made
+      </span>
+      <svg
+        width="36px"
+        height="36px"
+        strokeWidth="1.5"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        color="white"
+      >
+        <path
+          d="M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243"
+          stroke="white"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>
+      </svg>
+      <span className="whitespace-nowrap font-heading text-4xl uppercase">
+        Veteran Owned
+      </span>
+      <svg
+        width="36px"
+        height="36px"
+        strokeWidth="1.5"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        color="white"
+      >
+        <path
+          d="M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243"
+          stroke="white"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>
+      </svg>
     </div>
   );
 }
