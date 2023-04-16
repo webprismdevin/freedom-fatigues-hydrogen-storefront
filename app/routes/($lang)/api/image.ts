500445c3441bc46306a671a24f52734f0ae71d51
Devin Santamaria <devin@webprism.co>
2023-04-15 22:22:21 -0700

    remove types from image transformation

--- app/routes/($lang)/api/image.ts
+++ app/routes/($lang)/api/image.ts
@@ -1,15 +1,10 @@
-import type {LoaderFunction} from '@remix-run/server-runtime';
-import {
-  imageLoader,
-  MemoryCache,
-  pureTransformer,
-} from 'remix-image/serverPure';
+import {imageLoader, MemoryCache} from 'remix-image/serverPure';
 
 const config = {
   selfUrl: 'http://localhost:3000',
   cache: new MemoryCache(),
 };
 
-export const loader: LoaderFunction = ({request}) => {
+export const loader = ({request}) => {
   return imageLoader(config, request);
 };
