52c35f47d29187b0924a7da51f595d9b94f7133e
Devin Santamaria <devin@webprism.co>
2023-04-15 22:55:25 -0700

    doot

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,16 +1,4 @@
 import {Button} from './Button';
-import {useRef} from 'react';
-import {
-  motion,
-  useScroll,
-  useSpring,
-  useTransform,
-  MotionValue,
-} from 'framer-motion';
-
-function useParallax(value: MotionValue<number>, distance: number) {
-  return useTransform(value, [0, 1], [-distance, distance]);
-}
 
 export type Hero = {
   image: {
@@ -28,25 +16,20 @@ export type Hero = {
 };
 
 export function Hero({image, title, caption, cta, layout}: Hero) {
-  const ref = useRef<HTMLDivElement>(null);
-  const {scrollYProgress} = useScroll({target: ref});
-  const y = useParallax(scrollYProgress, 200);
-
   return (
     <div
       className={`flex-column align-center relative flex h-[700px] overflow-hidden p-8 lg:p-24 ${
         layout === 'right' && 'justify-end'
       } ${layout === 'center' && 'justify-center text-center'}`}
-      ref={ref}
     >
-      <motion.div className="absolute bottom-0 left-0 z-0 h-full w-full">
-        <motion.img
+      <div className="absolute bottom-0 left-0 z-0 h-full w-full">
+        <img
           src={image.url}
           className="top-300 absolute left-0 right-0 min-h-full min-w-full object-cover"
           alt={image.alt}
           loading={image.loading ? image.loading : 'lazy'}
         />
-      </motion.div>
+      </div>
       <div
         className={`${
           layout === 'right' && 'text-right'
