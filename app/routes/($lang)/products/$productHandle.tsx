a6042e70cbb131b07be13db905ce923d27e666a4
Devin Santamaria <devin@webprism.co>
2023-05-02 22:38:27 -0700

    readd redo

--- app/routes/($lang)/products/$productHandle.tsx
+++ app/routes/($lang)/products/$productHandle.tsx
@@ -188,11 +188,11 @@ export default function Product() {
     script.defer = true;
     document.body.appendChild(script);
 
-    // const redoScript = document.createElement('script');
-    // redoScript.src =
-    //   'http://shopify-extension.getredo.com/js/redo.js?widget_id=o5xzy8sv9eq3ma3';
-    // redoScript.async = true;
-    // document.body.appendChild(redoScript);
+    const redoScript = document.createElement('script');
+    redoScript.src =
+      'http://shopify-extension.getredo.com/js/redo.js?widget_id=o5xzy8sv9eq3ma3';
+    redoScript.async = true;
+    document.body.appendChild(redoScript);
   }, []);
 
   return (
