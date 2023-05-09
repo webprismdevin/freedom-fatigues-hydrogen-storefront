cd6a4d6db1dbecfe0aba8649d29659867860589e
Devin Santamaria <devin@webprism.co>
2023-05-09 11:18:51 -0700

    doot lhci

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,17 +4,10 @@ module.exports = {
       startServerCommand: 'npm run preview',
       pattern: '🚥 MiniOxygen server started',
       url: ['http://localhost:3000/'],
-      numberOfRuns: 1,
+      numberOfRuns: 3,
     },
     upload: {
       target: 'temporary-public-storage',
-    },
-    assert: {
-      preset: 'lighthouse:no-pwa',
-      'categories:performance': ['warn', {minScore: 0.9}],
-      'categories:accessibility': ['error', {minScore: 0.9}],
-      'categories:best-practices': ['error', {minScore: 0.9}],
-      'categories:seo': ['error', {minScore: 0.9}],
     },
   },
 };
