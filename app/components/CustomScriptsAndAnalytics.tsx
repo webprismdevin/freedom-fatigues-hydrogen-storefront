a778be38dec2d7b038f57444967429f3b1042bab
Devin Santamaria <devin@webprism.co>
2023-05-10 17:06:30 -0700

    add production flag to beta clarity

--- app/components/CustomScriptsAndAnalytics.tsx
+++ app/components/CustomScriptsAndAnalytics.tsx
@@ -90,18 +90,20 @@ export function CustomScriptsAndAnalytics() {
   }, []);
   //clarity for early testing
   useEffect(() => {
-    (function (c, l, a, r, i, t, y) {
-      c[a] =
-        c[a] ||
-        function () {
-          (c[a].q = c[a].q || []).push(arguments);
-        };
-      t = l.createElement(r);
-      t.async = 1;
-      t.src = 'https://www.clarity.ms/tag/' + i;
-      y = l.getElementsByTagName(r)[0];
-      y.parentNode.insertBefore(t, y);
-    })(window, document, 'clarity', 'script', 'h1zr8912jr');
+    if (process.env.NODE_ENV === 'production') {
+      (function (c, l, a, r, i, t, y) {
+        c[a] =
+          c[a] ||
+          function () {
+            (c[a].q = c[a].q || []).push(arguments);
+          };
+        t = l.createElement(r);
+        t.async = 1;
+        t.src = 'https://www.clarity.ms/tag/' + i;
+        y = l.getElementsByTagName(r)[0];
+        y.parentNode.insertBefore(t, y);
+      })(window, document, 'clarity', 'script', 'h1zr8912jr');
+    }
   }, []);
 
   useScript(
