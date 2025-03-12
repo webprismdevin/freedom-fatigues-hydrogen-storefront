04a5ddbc8b5bc9952d794667f81a1a66a0188440
Devin Santamaria <devin@webprism.co>
2025-03-12 01:36:33 -0700

    Fix imports for Hydrogen 2024.10.0 compatibility

--- app/lib/client.ts
+++ app/lib/client.ts
@@ -1,4 +1,4 @@
-import {create} from '@shopify/hydrogen';
+import {createStorefrontClient} from '@shopify/hydrogen';
 export const client = createStorefrontClient({
   // load environment variables according to your framework and runtime
   storeDomain: process.env.PUBLIC_STORE_DOMAIN,
